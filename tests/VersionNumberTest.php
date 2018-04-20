95890a0f372d48c795c3e7a825c08c8c0eb2fadd
Holger Woltersdorf <hw@hollo.me>
2018-04-20 14:59:19 +0200

    Add version number test skeleton

---
+++ tests/VersionNumberTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace In2it\Masterclass\Test;
+
+use PHPUnit\Framework\TestCase;
+
+final class VersionNumberTest extends TestCase
+{
+	public function testCanGetIncrementedVersionNumber() : void
+	{
+
+	}
+
+	public function testIsVersionNumberGreaterThanOtherVersionNumber() : void
+	{
+
+	}
+
+	public function testCanGetJsonSerializedVersionNumber() : void
+	{
+
+	}
+}
